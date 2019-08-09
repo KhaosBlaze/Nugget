d1a74cfaf2b7f76333f1c5fc666773d37fc09a7a
KhaosBlaze <khaosblaze@gmail.com>
2019-08-09 17:53:32 -0400

    Update index.js

--- soundboard/index.js
+++ soundboard/index.js
@@ -50,6 +50,7 @@ nugget.on('message', async message => {
 async function execute(message, serverQueue) {
  const args = message.content.split(' ');
  const voiceChannel = message.member.voiceChannel;
+ console.log(voiceChannel);
  const permissions = voiceChannel.permissionsFor(message.client.user);
  if (!permissions.has('CONNECT') || !permissions.has('SPEAK')) {
   return message.channel.send('Oink Oink Oink Oink Oink Oink');
@@ -78,22 +79,22 @@ async function execute(message, serverQueue) {
 
   try {
    // Here we try to join the voicechat and save our connection into our object.
-   var connection = await voiceChannel.join();
-   queueContruct.connection = connection;
-   // Calling the play function to start a song
-   play(message.guild, queueContruct.songs[0]);
-  } catch (err) {
-   // Printing the error message if the bot fails to join the voicechat
-   console.log(err);
-   queue.delete(message.guild.id);
-   return message.channel.send(err);
+    var connection = await voiceChannel.join();
+    queueContruct.connection = connection;
+    // Calling the play function to start a song
+    play(message.guild, queueContruct.songs[0]);
+   } catch (err) {
+    // Printing the error message if the bot fails to join the voicechat
+    console.log(err);
+    queue.delete(message.guild.id);
+    return message.channel.send(err);
+   }
+  }else {
+   serverQueue.songs.push(song);
+   console.log(serverQueue.songs);
+   return message.channel.send(`${song.title} oink oink added oink oink queue!`);
   }
- }else {
-  serverQueue.songs.push(song);
-  console.log(serverQueue.songs);
-  return message.channel.send(`${song.title} oink oink added oink oink queue!`);
  }
-}
 
  function play(guild, song) {
   const serverQueue = queue.get(guild.id);
