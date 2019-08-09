4f143ae92330b0085608231b0b4574498516ea41
KhaosBlaze <khaosblaze@gmail.com>
2019-08-09 16:38:25 -0400

    Update index.js

--- soundboard/index.js
+++ soundboard/index.js
@@ -24,5 +24,79 @@ nugget.once('disconnect', () => {
 });
 
 nugget.on('message', async message => {
-  
+ //We don't care if the message is from Nugget or 
+ if (message.author.bot) return;
+ if (!message.content.startsWith(prefix)) return;
+
+ //This is temporary. Nugget will be controlled by Buttons
+ const serverQueue = queue.get(message.guild.id);
+
+ if (message.content.startsWith(`${prefix}play`)) {
+  execute(message, serverQueue);
+  return;
+ } else if (message.content.startsWith(`${prefix}skip`)) {
+  skip(message, serverQueue);
+  return;
+ } else if (message.content.startsWith(`${prefix}stop`)) {
+  stop(message, serverQueue);
+  return;
+ } else {
+  message.channel.send('You need to enter a valid command!')
+ }
 });
+
+//Oh hello execution
+async function execute(message, serverQueue) {
+ const args = message.content.split(' ');
+ const voiceChannel = message.member.voiceChannel;
+ if (!permissions.has('CONNECT') || !permissions.has('SPEAK')) {
+  return message.channel.send('Oink Oink Oink Oink Oink Oink');
+ }
+
+ const songInfo = await ytdl.getInfo(args[1]);
+ const song = {
+  title: songInfo.title,
+  url: songInfo.video_url,
+ };
+ if (!serverQueue) {
+  // Creating the contract for our queue
+  const queueContruct = {
+   textChannel: message.channel,
+   voiceChannel: voiceChannel,
+   connection: null,
+   songs: [],
+   volume: 5,
+   playing: true,
+  };
+  // Setting the queue using our contract
+  queue.set(message.guild.id, queueContruct);
+  // Pushing the song to our songs array
+  queueContruct.songs.push(song);
+
+  try {
+   // Here we try to join the voicechat and save our connection into our object.
+   var connection = await voiceChannel.join();
+   queueContruct.connection = connection;
+   // Calling the play function to start a song
+   play(message.guild, queueContruct.songs[0]);
+  } catch (err) {
+   // Printing the error message if the bot fails to join the voicechat
+   console.log(err);
+   queue.delete(message.guild.id);
+   return message.channel.send(err);
+  }
+ }else {
+  serverQueue.songs.push(song);
+  console.log(serverQueue.songs);
+  return message.channel.send(`${song.title} oink oink added oink oink queue!`);
+ }
+}
+
+function play(guild, song) {
+ const serverQueue = queue.get(guild.id);
+ if (!song) {
+  serverQueue.voiceChannel.leave();
+  queue.delete(guild.id);
+  return;
+ }
+}
