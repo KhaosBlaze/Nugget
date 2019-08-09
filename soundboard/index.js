3d112b631d2d933326ed91df97db42bc9b133907
KhaosBlaze <khaosblaze@gmail.com>
2019-08-09 16:08:40 -0400

    Update index.js

--- soundboard/index.js
+++ soundboard/index.js
@@ -1,6 +1,23 @@
+//Import Discord/YTDL && configs
+
 const Discord = require('discord.js');
 const {
 	prefix,
 	token,
 } = require('./config.json');
 const ytdl = require('ytdl-core');
+
+//Connecting to the Matrix
+const client = new Discord.Client();
+client.login(token);
+
+//Console logging
+client.once('ready', () => {
+ console.log('Ready!');
+});
+client.once('reconnecting', () => {
+ console.log('Reconnecting!');
+});
+client.once('disconnect', () => {
+ console.log('Disconnect!');
+});
